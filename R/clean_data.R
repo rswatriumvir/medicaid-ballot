4b586627f1b2ab1ef32c6a5d153b70a0e2cbec7f
rswatriumvir <roswain2002@gmail.com>
2021-05-20 13:42:03 -0400

    First step in cleaning data for Oklahoma

---
+++ R/clean_data.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+#load libraries
+
+df=read_csv("raw_data/oklahoma_june_2020_elec.csv")
+#load data
+
+okl=df %>% 
+  filter(race_description=="STATE QUESTION NO. 802 INITIATIVE PETITION NO. 419")
+
