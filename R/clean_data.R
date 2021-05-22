6d6819a4802094d1b9ec5011a0a2495c360ed6e4
rswatriumvir <roswain2002@gmail.com>
2021-05-22 01:37:29 -0400

    Selected columns and merged rows for Oklahoma

--- R/clean_data.R
+++ R/clean_data.R
@@ -5,5 +5,8 @@ df=read_csv("raw_data/oklahoma_june_2020_elec.csv")
 #load data
 
 okl=df %>% 
-  filter(race_description=="STATE QUESTION NO. 802 INITIATIVE PETITION NO. 419")
-#apples
+  filter(race_description=="STATE QUESTION NO. 802 INITIATIVE PETITION NO. 419") %>%
+  select(elec_date, county, cand_name, cand_tot_votes) %>%
+  pivot_wider(names_from=cand_name, values_from=cand_tot_votes)
+
+
