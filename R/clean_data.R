8f0f7a995b763ed4a6f10de219889ec171ceb764
Juan Tellez <juan.f.tellez@gmail.com>
2021-05-28 09:59:59 -0400

    cleaned up file, dealt with issue reading in excel files

--- R/clean_data.R
+++ R/clean_data.R
@@ -1,20 +1,19 @@
+# load libraries --------------------------------------------------------------------
 library(tidyverse)
 library(readxl)
-library(dplyr)
 library(tabulizer)
 library(tabulizerjars)
-#load libraries
 
-okl=read_csv("raw_data/oklahoma_june_2020_elec.csv")
-id=read_excel("raw_data/idaho_2018_results.xls", sheet="Props - Voting Stats")
-mt=read_excel("raw_data/montana_2018_tobacco.xlsx")
-ut=extract_tables("raw_data/2018 General Election Canvass Utah.pdf")
-nb=extract_tables("raw_data/nebraska_2018_elec.pdf")
-nb_dt=nb[318] ##list element with proposition data for Nebraska
-nb_dt1=do.call(rbind, nb_dt)
-#load data
 
-okl_dt=okl %>% 
+
+# deal with excel -------------------------------------------------------------------
+
+
+
+## oklahoma
+okl = read_csv("raw_data/oklahoma_june_2020_elec.csv")
+
+okl_dt = okl %>% 
   filter(race_description=="STATE QUESTION NO. 802 INITIATIVE PETITION NO. 419") %>%
   select(elec_date, county, cand_name, cand_tot_votes) %>%
   pivot_wider(names_from=cand_name, values_from=cand_tot_votes) %>%
@@ -22,7 +21,25 @@ okl_dt=okl %>%
   mutate(Total_Votes=`FOR THE PROPOSAL - YES`+ `AGAINST THE PROPOSAL - NO`) %>%
   mutate(Share_For=(`FOR THE PROPOSAL - YES`/Total_Votes)*100) %>%
   mutate(Share_Against=(`AGAINST THE PROPOSAL - NO`/Total_Votes)*100)
-#cleaning oklahoma data
+
+
+## idaho
+
+# original file has useless first five rows; skip them
+id = read_excel("raw_data/idaho_2018_results.xls", 
+                sheet="Props - Voting Stats", 
+                skip = 5)
+
+
+
+## montana
+
+# original file has useless first 6 rows; skip them
+mt = read_excel("raw_data/montana_2018_tobacco.xlsx", 
+                skip = 6)
+
+
+
 
 mt_dt=mt %>%
   select(...2,...3,...4) %>%
@@ -37,12 +54,19 @@ mt_dt=mt %>%
   mutate(Share_For=Votes_For/Total_Votes) %>%
   mutate(Share_Against=Votes_Against/Total_Votes)
 
-id_dt=id %>%
-  
 
   
 
+  
+
+# deal with PDFs --------------------------------------------------------------------
 
 
+
+ut=extract_tables("raw_data/2018 General Election Canvass Utah.pdf")
+nb=extract_tables("raw_data/nebraska_2018_elec.pdf")
+nb_dt=nb[318] ##list element with proposition data for Nebraska
+nb_dt1=do.call(rbind, nb_dt)
+#load data
 
 
