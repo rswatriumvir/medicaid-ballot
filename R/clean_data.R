606daded85f20c61dc09ec116d722a3107505dea
rswatriumvir <roswain2002@gmail.com>
2021-05-30 00:12:20 -0400

    Update clean_data.R
    Completed Montana and Idaho

--- R/clean_data.R
+++ R/clean_data.R
@@ -14,13 +14,15 @@ library(tabulizerjars)
 okl = read_csv("raw_data/oklahoma_june_2020_elec.csv")
 
 okl_dt = okl %>% 
-  filter(race_description=="STATE QUESTION NO. 802 INITIATIVE PETITION NO. 419") %>%
+  filter(race_description == "STATE QUESTION NO. 802 INITIATIVE PETITION NO. 419") %>%
   select(elec_date, county, cand_name, cand_tot_votes) %>%
   pivot_wider(names_from=cand_name, values_from=cand_tot_votes) %>%
-  mutate(State="Oklahoma") %>%
-  mutate(Total_Votes=`FOR THE PROPOSAL - YES`+ `AGAINST THE PROPOSAL - NO`) %>%
-  mutate(Share_For=(`FOR THE PROPOSAL - YES`/Total_Votes)*100) %>%
-  mutate(Share_Against=(`AGAINST THE PROPOSAL - NO`/Total_Votes)*100) 
+  rename(votes_for =`FOR THE PROPOSAL - YES`) %>%
+  rename(votes_against = `AGAINST THE PROPOSAL - NO`) %>%
+  mutate(state = "Oklahoma") %>%
+  mutate(total_votes = votes_for + votes_against) %>%
+  mutate(share_for = (votes_for / total_votes) * 100) %>%
+  mutate(share_against = (votes_against / total_votes) * 100) 
 
 
 ## idaho
@@ -30,6 +32,20 @@ id = read_excel("raw_data/idaho_2018_results.xls",
                 sheet="Props - Voting Stats", 
                 skip = 5)
 
+id_dt = id %>%
+  select(Counties, YES...4, NO...5) %>%
+  rename(votes_for = YES...4) %>%
+  rename(votes_against = NO...5) %>%
+  mutate(state = "Idaho") %>%
+  mutate(total_votes = votes_for + votes_against) %>%
+  mutate(share_for = (votes_for / total_votes) * 100) %>%
+  mutate(share_against = (votes_against / total_votes) * 100) %>%
+  mutate(elec_date = '11/06/2018')
+  
+  
+  
+  
+  
 
 
 ## montana
@@ -41,14 +57,15 @@ mt = read_excel("raw_data/montana_2018_tobacco.xlsx",
 
 
 
-mt_dt=mt %>%
+mt_dt = mt %>%
   select(County,`YES on INITIATIVE NO. 185`,`NO on INITIATIVE NO. 185`) %>%
-  mutate(State="Montana") %>%
-  rename(Votes_For=`YES on INITIATIVE NO. 185`) %>%
-  rename(Votes_Against=`NO on INITIATIVE NO. 185`) %>%
-  mutate(Total_Votes=Votes_For+Votes_Against) %>%
-  mutate(Share_For=(Votes_For/Total_Votes)*100) %>%
-  mutate(Share_Against=(Votes_Against/Total_Votes)*100)
+  mutate(State = "Montana") %>%
+  mutate(elec_date = '11/06/2018') %>%
+  rename(votes_for=`YES on INITIATIVE NO. 185`) %>%
+  rename(votes_against=`NO on INITIATIVE NO. 185`) %>%
+  mutate(total_votes = votes_for + votes_against) %>%
+  mutate(share_for = (votes_for / total_votes) * 100) %>%
+  mutate(share_against = (votes_against / total_votes) * 100)
 
 
   
