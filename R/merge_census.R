f96f774ba2b8ffec334bd351f6ed2f91fd680ba0
rswatriumvir <roswain2002@gmail.com>
2021-06-03 15:31:21 -0400

    Update merge_census.R

--- R/merge_census.R
+++ R/merge_census.R
@@ -4,3 +4,15 @@ library(tidyverse)
 
 # load data
 df = read_rds("output_data/aca_refs.rds")
+
+## adding ACS demographic and educational datasets -----------------------------------
+
+'acs_demo_og = read_csv("raw_data/acs_demographics.csv") #original data from ACS demographics
+acs_demo_og = as.data.frame(t(acs_demo_og)) #switching rows and columns
+setDT(acs_demo_og, keep.rownames = TRUE)
+acs_demo = acs_demo_og %>%
+  row_to_names(row_number = 1) 
+acs_demo$Label
+filter((grepl("Percent", Label) == TRUE))'
+
+'acs_demo[[2]]'
