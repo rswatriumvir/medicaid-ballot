9d2aac00cc5e136d766cc80b506c013451d268af
rswatriumvir <roswain2002@gmail.com>
2021-06-14 22:28:23 -0400

    Update make_graphs.R

--- R/make_graphs.R
+++ R/make_graphs.R
@@ -17,8 +17,9 @@ df = read_rds("output_data/aca_refs_final.rds")
 
 ot_list = df %>%
   mutate(yes_diff = share_for - democrat) %>% #see how left-wing policy support compares to left-wing party support
-  filter((yes_diff > 0) == TRUE) %>% #where aca support outperformed dem support
-  filter(state=="Maine") 
+  filter((yes_diff > 0) == TRUE) %>%  #where aca support outperformed dem support
+  filter(state=="Idaho")
+
 
 ggplot(ot_list, aes(x = democrat, y = yes_diff)) + geom_point()+ geom_text(label = ot_list$county) + labs(x = "Democratic Vote Share, 2016 Presidential", y = "Margin Between Yes Voteand 2016", title = "Yes Margin Vs 2016 Dem Presidential Vote")
 
